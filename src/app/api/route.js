90eb78425b683583257201987b2a97b9ef318f2c
054Prapawit <66309010054@cmtc.ac.th>
2024-07-25 14:20:39 +0700

    new

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,10 +1,23 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
 export async function GET() {
     try {
-        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+        const result = await client.query('SELECT * FROM tbl_users');
+        return new Response(JSON.stringify(result.rows), {
             status: 200,
             headers: { "Content-Type": "application/json" },
         });
     } catch (error) {
+
         return new Response(JSON.stringify({ error: "Internal Server Error" }), {
             status: 500,
             headers: { "Content-Type": "application/json" },
