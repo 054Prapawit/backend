aafc2c96f3bc9b6e6746f848732d9ff7975939f6
054Prapawit <66309010054@cmtc.ac.th>
2024-08-01 14:08:30 +0700

    Update route.js

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -20,7 +20,7 @@ export async function GET(request, { params }) {
           headers: { "Content-Type": "application/json" },
         });
       } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), {
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
           status: 500,
           headers: { "Content-Type": "application/json" },
         });
